aed434c328bd08ea9b2f20ee4738825581d993e8
jonaslozys <jlozys@gmail.com>
2019-02-16 12:53:45 +0200

    added bookings table

--- sql/barbershop.sql
+++ sql/barbershop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 16, 2019 at 10:30 AM
+-- Generation Time: Feb 16, 2019 at 11:52 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.1
 
@@ -42,6 +42,27 @@ INSERT INTO `barbers` (`id`, `name`, `bio`) VALUES
 (1, 'Jonas', 'Professional with 10 years experience.'),
 (2, 'Petras', 'Newbie, messes up often :p');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `staff_id` int(11) NOT NULL,
+  `appointment_id` int(11) NOT NULL,
+  `customer_name` varchar(255) NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bookings`
+--
+
+INSERT INTO `bookings` (`staff_id`, `appointment_id`, `customer_name`, `date`) VALUES
+(2, 1, 'Ponas Jonas', '2019-02-16 13:30:00'),
+(2, 2, 'Vardenis Pavardauskas', '2019-02-16 14:00:00');
+
 --
 -- Indexes for dumped tables
 --
@@ -53,6 +74,13 @@ ALTER TABLE `barbers`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`appointment_id`),
+  ADD KEY `staff_id` (`staff_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -61,6 +89,22 @@ ALTER TABLE `barbers`
 --
 ALTER TABLE `barbers`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `bookings`
+--
+ALTER TABLE `bookings`
+  MODIFY `appointment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`staff_id`) REFERENCES `barbers` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ views/select_time.php
@@ -0,0 +1,18 @@
+<?php 
+    if(isset($_GET["submit"])){
+        echo $_GET["date"];
+    } else {
+        echo "errror";
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Best barbershop in town</title>
+    </head>
+
+    <body>
+        <h2>Select one of available times</h2>
+    </body>
+</html>
