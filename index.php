c54256a89ff2979d8f9e385e66a92a754e1e7848
jonaslozys <jlozys@gmail.com>
2019-02-16 11:17:25 +0200

    Initial commit, home page added

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Best barbershop in town</title>
+    </head>
+
+    <body>
+        <button><a href="./views/select_barber.php">Book an appointment</a></button>
+        <button><a href="">Staff login</a></button>
+    </body>
+</html>
