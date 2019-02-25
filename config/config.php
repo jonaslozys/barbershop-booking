25e8b72a3ad1ff7b6e7506cb80bf8a4ec6d0103b
jonaslozys <jlozys@gmail.com>
2019-02-25 12:54:35 +0200

    edited db config for heroku's clearDB

--- config/config.php
+++ config/config.php
@@ -4,9 +4,16 @@
     date_default_timezone_set('Europe/Vilnius');
 
     // database credentials config
-    define("DB_HOST", "localhost");
-    define("DB_USERNAME", "root");
-    define("DB_PASSWORD", "");
-    define("DB_NAME", "barbershop");
-    
+
+
+    // config for clearDB
+    define("DB_HOST", "us-cdbr-iron-east-03.cleardb.net");
+    define("DB_USERNAME", "b15f3c28207aa8");
+    define("DB_PASSWORD", "4aebefa5");
+    define("DB_NAME", "heroku_ecfddbe5865ae34");
+  
+    // define("DB_HOST", "localhost");
+    // define("DB_USERNAME", "root");
+    // define("DB_PASSWORD", "");
+    // define("DB_NAME", "barbershop");
 ?>
