ac7cbbb3e155ff3c65f1b828c52ac4ade2da4a55
jonaslozys <jlozys@gmail.com>
2019-02-25 13:36:43 +0200

    fixed db config

--- config/config.php
+++ config/config.php
@@ -9,14 +9,14 @@
     // database credentials config
 
 
-    // config for clearDB
-    // define("DB_HOST", "us-cdbr-iron-east-03.cleardb.net");
-    // define("DB_USERNAME", "b15f3c28207aa8");
-    // define("DB_PASSWORD", "4aebefa5");
-    // define("DB_NAME", "heroku_ecfddbe5865ae34");
+    config for clearDB
+    define("DB_HOST", "us-cdbr-iron-east-03.cleardb.net");
+    define("DB_USERNAME", "b15f3c28207aa8");
+    define("DB_PASSWORD", "4aebefa5");
+    define("DB_NAME", "heroku_ecfddbe5865ae34");
   
-    define("DB_HOST", "localhost");
-    define("DB_USERNAME", "root");
-    define("DB_PASSWORD", "");
-    define("DB_NAME", "barbershop");
+    // define("DB_HOST", "localhost");
+    // define("DB_USERNAME", "root");
+    // define("DB_PASSWORD", "");
+    // define("DB_NAME", "barbershop");
 ?>
